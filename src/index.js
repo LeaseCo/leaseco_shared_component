d76815c99ec7e1d25fff8022fe8c97513eb8c521
napalm272 <pwei@tetrascience.com>
2017-12-05 10:07:21 -0800

    LEASE-25 fix es6 build

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export CustomerForm from './CustomerForm';
+export FieldGroup from './FieldGroup';
+export {BraintreeForm, DefaultCreditCardSection} from './lc-braintree';
+export LeaseDetails from './lease-details/LeaseDetails';
+export LeaseDocs from './lease-details/LeaseDocs';
+export LeasePaymentDetails from './lease-details/payment-details';
+export LeaseOrderDetails from './lease-details/order-details';
+export LeaseEarlyPayoff from './lease-details/early-payoff';
